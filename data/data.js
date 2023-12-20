c1a408614b12d913bdccdbc9a4d00895abd84e3d
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-12-20 11:32:17 +0100

    new file:   data/data.js

---
+++ data/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+        title: 'The Blueprint',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-050-Jay-Z-The-Blueprint.jpg?w=1000',
+        artist: 'Jay-Z',
+        genre: 'Hip-Hop/Rap',
+        duration: '1h and 7min',
+    },
+    {
+        title: 'Aquemini',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-049-Outkast-Aquemini.jpg?w=1000',
+        artist: 'Outcast',
+        genre: 'Musical /Funk /Soul /R&B /Rock.',
+        duration: '74 min',
+    },
+    {
+        title: 'Legend',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-048-Bob-Marley-and-the-Wailers-Legend.jpg?w=1000',
+        artist: 'Bob Marley',
+        genre: 'Raggae',
+        duration: '72 min',
+    },
+    {
+        title: 'Let It Bleed',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-041-The-Rolling-Stones-Let-It-Bleed.jpg?w=1000',
+        artist: 'The Rolling Stones',
+        genre: 'Blues, Country, & Gospel',
+        duration: '42 min',
+    },
+    {
+        title: 'Kid A',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-020-Radiohead-KID-A.jpg?w=1000',
+        artist: 'Radiohead',
+        genre: 'Rock, Electronic & Art Rock',
+        duration: '49 min',
+    },
+    {
+        title: 'Rubber Soul',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-035-The-Beatles-Rubber-Soul.jpg?w=1000',
+        artist: 'The Beatles',
+        genre: 'Folk&Rock',
+        duration: '34 min',
+    },
+    {
+        title: 'Innervisions',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-034-Stevie-Wonder-Innervisions.jpg?w=1000',
+        artist: 'Stevie Wonder',
+        genre: 'Folk&Rock',
+        duration: '34 min',
+    },
+    {
+        title: 'Back to Black',
+        image: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-033-Amy-Winehouse-Back-to-Black.jpg?w=1000',
+        artist: 'Amy Winehouse',
+        genre: 'R&B & Jazz',
+        duration: '34 min',
+    },
+]
+
+export default data;
